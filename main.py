1ce78207b7164676b10f1cb876e66c6e7e1eb0eb
Omega050 <mauro-jr50@hotmail.com>
2024-12-02 23:20:07 -0300

    chore: clean

--- main.py
+++ main.py
@@ -131,7 +131,6 @@ class AttendanceSystem:
     async def periodic_tasks(self):
         """Run periodic maintenance tasks"""
         while self.running:
-            print("CALLEEECALLEEECALLEEECALLEEECALLEEECALLEEECALLEEECALLEEE")
             try:
                 current_time = datetime.now()
 
